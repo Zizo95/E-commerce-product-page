f690f2a2cc92f32083f97f47104136dac11abe26
Zizo95 <147083219+Zizo95@users.noreply.github.com>
2024-07-23 14:33:00 +0200

    updated script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- c
+ const cart = documnet.querySelector(".cart");
+ const cartBox = document.querySelector(".cart-box");
+ const imageOne = document.querySelector(".image-1");
+ const imageTwo = document.querySelector(".image-2");
+ const imageThree = document.querySelector(".image-3");
+ const imageFour = document.querySelector(".image-4");
+ const iconMinus = document.querySelector(".icon-minus");
+ const iconPlus = document.querySelector(".icon-plus");
+ const button = document.querySelector("button");
+ 
